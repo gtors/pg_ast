46f30fbd051d0c1688baf1bf19c51e3e4ff37a55
gtors <gtors.potato@gmail.com>
2019-02-14 01:01:57 +0300

    Partialy implement AlterObjectDepends

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,37 @@
-pub enum AlterNode {
-    AlterCollation(AlterCollationStmt),
+use crate::enums::ObjectKind;
+use std::convert::From;
+
+macro_rules! impl_from {
+    ($enum_:ident, $kind:ident, $struct_:ident) => {
+        impl From<$struct_> for $enum_ {
+            fn from(s: $struct_) -> Self {
+                $enum_::$kind(s)
+            }
+        }
+    };
 }
 
+pub enum AlterNode {
+    AlterCollation(AlterCollationStmt),
+    AlterObjectDepends(AlterObjectDependsStmt),
+}
+macro_rules! an_impl_from {
+    ($kind:ident, $struct_:ident) => {
+        impl_from!(AlterNode, $kind, $struct_);
+    };
+}
+an_impl_from!(AlterCollation, AlterCollationStmt);
+an_impl_from!(AlterObjectDepends, AlterObjectDependsStmt);
+
+pub enum AlterObject {
+    QualifiedName(QualifiedName),
+}
+macro_rules! ao_impl_from {
+    ($kind:ident, $struct_:ident) => {
+        impl_from!(AlterObject, $kind, $struct_);
+    };
+}
+ao_impl_from!(QualifiedName, QualifiedName);
 
 #[derive(Debug)]
 pub struct QualifiedName {
@@ -9,7 +39,17 @@ pub struct QualifiedName {
     pub name: String,
 }
 
-
 pub struct AlterCollationStmt {
     pub collation_name: QualifiedName,
+}
+
+pub struct AlterObjectDependsStmt {
+    /// OBJECT_FUNCTION, OBJECT_TRIGGER, etc
+    pub object_kind: ObjectKind,
+    /// in case a table is involved
+    pub relation: Option<QualifiedName>,
+    /// name of the object
+    pub object: Option<AlterObject>,
+    /// extension name
+    pub extension_name: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use lalrpop_util::{lalrpop_mod};
+use lalrpop_util::lalrpop_mod;
 
 mod ast;
+mod enums;
 
-use ast::{AlterNode};
+use ast::AlterNode;
 
 lalrpop_mod!(pub pg);
-
 
 fn main() {
     let sql = r#"ALTER COLLATION "public".any_name REFRESH VERSION"#;
     let alter_node = pg::AlterParser::new().parse(sql).unwrap();
 
     match alter_node {
-       AlterNode::AlterCollation(x) => println!("{:?}", x.collation_name),
-       _ => println!("Not ok")
+        AlterNode::AlterCollation(x) => println!("{:?}", x.collation_name),
+        _ => println!("Not ok"),
     }
 }
